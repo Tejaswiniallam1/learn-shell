2ca9c1958412f9e540359e5c78f3e86ee1310439
tejudevops <saitejaswini8121@gmail.com>
2024-11-17 18:28:28 +0530

    adding colors

--- print.sh
+++ print.sh
@@ -1,7 +1,14 @@
-# 0,1,2,3,,,,n,*,#
+# we have only 6 colors in shell
 
-echo Name of the script -$0
-echo First Argument -$1
-echo second Argument -$2
-echo all Arguments -$*
-echo no of argument -$#
+Red  -31
+Green -32
+yellow -33
+blue -34
+margita -35
+cyan -36
+
+#synatax: -e  "\e[CodeofColormMessageinthecode\e[0m"
+#Example :echo -e "\e[35m Disable nodejs existing version\e[0m"
+
+-e = is used to enable \e
+[0m = is used to exit
