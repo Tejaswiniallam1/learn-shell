12ed46b81cffbfecf3bfd19d5019f0d63f191b87
tejudevops <saitejaswini8121@gmail.com>
2024-11-20 20:12:52 +0530

    adding colors

---
+++ 03.Functions.sh
@@ -0,0 +1,12 @@
+# Function
+sample1() {
+  #Inside the Function
+  echo "Hello World"
+}
+
+#Main Program
+
+sample1
+
+#Declared variable in main program can be accessed in function and its vice versa
+#Declared variable in main program can be overwritten by function and its vice versa
---
+++ 1.print.sh
@@ -0,0 +1,23 @@
+# we have only 6 colors in shell
+
+Red  -31
+Green -32
+yellow -33
+blue -34
+magenta -35
+cyan -36
+
+#synatax: -e  "\e[CodeofColormMessageinthecode\e[0m"
+
+# -e -> Enable \e
+#\e[COLCODEm (\e[31m) ->Enable the color
+#\e[0m ->Disable Color
+
+#Note :Input needs to be in double quotes (single quotes is also acceptable)
+
+echo -e "\e[31m Disable nodejs existing version\e[0m"
+echo -e "\e[32m Disable nodejs existing version\e[0m"
+echo -e "\e[33m Disable nodejs existing version\e[0m"
+echo -e "\e[34m Disable nodejs existing version\e[0m"
+echo -e "\e[35m Disable nodejs existing version\e[0m"
+echo -e "\e[36m Disable nodejs existing version\e[0m"
