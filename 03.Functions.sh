fda8af5d68efba09fceddea77299fc2f9d717f94
tejudevops <saitejaswini8121@gmail.com>
2024-11-20 23:53:37 +0530

    adding colors

--- 03.Functions.sh
+++ 03.Functions.sh
@@ -19,3 +19,12 @@ sample_sum(){
   echo "addition of $1 and $2 is $(($1+$2))"
 }
 sample_sum 100 200
+
+# Inside main program all special vars(0-n,* ,#) which are passed to scripts are not accesible inside function
+
+sample2() {
+  echo Hello1
+  return
+  echo Hello2
+}
+sample2
