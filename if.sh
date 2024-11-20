87f226ec437325b33fa643302094eee801fba29d
tejudevops <saitejaswini8121@gmail.com>
2024-11-21 01:15:21 +0530

    if

---
+++ if.sh
@@ -0,0 +1,55 @@
+#Simple if
+if [ expression ]; then
+  commands
+fi
+#If Else
+if[ expression ]; then
+    commands
+ else
+   commands
+fi
+
+
+else if [ expression1 ]; then
+  commands1
+elif [ expression2 ]; then
+  commands2
+else
+    commands
+fi
+
+#Expressions
+1.Strings Checks
+[ string (operator) string]
+
+operators
+= ,==,-> Check if both the strings are same
+!= -> Check if both strings are not same.
+
+2.Number Checks.
+
+
+3. File Checks
+
+Example:
+input =$1
+if[ $input = devops]; then
+  echo Welcome to devops Training
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
