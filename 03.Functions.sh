68cf052e0d315c6543f90d72213ac9fcb12c56b4
tejudevops <saitejaswini8121@gmail.com>
2024-11-20 22:52:04 +0530

    adding colors

--- 03.Functions.sh
+++ 03.Functions.sh
@@ -10,3 +10,11 @@ sample1
 
 #Declared variable in main program can be accessed in function and its vice versa
 #Declared variable in main program can be overwritten by function and its vice versa
+
+#Along with this function have special Variables , We can pass  arguments to  function.
+
+#sample1 100 200
+
+sample_sum(){
+  echo "addition of $1 and $2 is (($1+$2))"
+}
