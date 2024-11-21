8bc2aa6cac5ef01b15f053b8cbeea0c2cc5f2185
tejudevops <saitejaswini8121@gmail.com>
2024-11-21 20:41:30 +0530

    if

---
+++ loops.sh
@@ -0,0 +1,15 @@
+#Two loop commands(2 additional commands)
+
+#while loop
+
+#while [ expression ]; do
+# commands
+#done
+#if you want to control a loop with expression then while has to be choice
+
+
+#For Loop
+#for somevar in values(values1 value2); do
+#commands
+#done
+#if you want to control the loop based on values then for has to be used.
