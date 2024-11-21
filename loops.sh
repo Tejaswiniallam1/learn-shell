dc2dedef4a7fc481b9e021f65f6ab17091846f10
tejudevops <saitejaswini8121@gmail.com>
2024-11-21 20:46:58 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -7,6 +7,14 @@
 #done
 #if you want to control a loop with expression then while has to be choice
 
+#example
+i=10
+while [ "$i" -gt 0]do
+  echo Hello World
+  i=$(($i-1))
+  done
+
+
 
 #For Loop
 #for somevar in values(values1 value2); do
